b0ae76283a5ab53e572bb7bccf21dfc008d0e4da
João Vitor Mauricio Cruz <82549355+JoaoVitorCruz@users.noreply.github.com>
2021-09-28 13:40:10 -0300

    footer e aviso

--- src/components/videoSelector.jsx
+++ src/components/videoSelector.jsx
@@ -12,9 +12,9 @@ function VideoSelector(props) {
         window.open(`/api/download/${type}/${finalURL}`)
     }
 
-    return <div className="flex rounded-lg overflow-hidden w-full bg-gray-800 text-white mb-5">
+    return <div className="flex entrar rounded-lg overflow-hidden w-full bg-gray-800 text-white md:mx-auto md:w-8/12 mb-5">
         <div className="w-4/12 my-auto">
-            <img src={thumb} className="w-full " alt="Thumbnail" />
+            <img src={thumb} className="w-full transition rounded-lg" alt="Thumbnail" />
             <div className="text-center py-1 px-3 bg-gray-700 opacity-1 rounded-full mx-auto w-1/2 text-white my-2">{duration}</div>
         </div>
         
@@ -25,7 +25,7 @@ function VideoSelector(props) {
             <h4>{author}</h4>
         </div>
             
-            <div className="my-2">
+            <div className="my-2 ml-2">
                 <label>Baixar: </label>
                 <button 
                 className="px-4 py-1 border rounded-full shadow-md shadow color-gray-100 mx-1 hover:bg-gray-100 hover:text-gray-800 transition"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import SearchBar from '../components/searchBar'
 import VideoSelector from '../components/videoSelector'
 import { useState } from 'react'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import WhatsappIcon from '@material-ui/icons/Whatsapp'
+import InstagramIcon from '@material-ui/icons/Instagram'
 
 export default function Home() {
   let [searchResults, setSearchResult] = useState(false)
@@ -13,28 +16,62 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-gray-900 h-screen w-screen static">
-        <header className="flex justify-center bg-gray-900">
+        <header className="flex justify-center bg-gray-800">
           <h2 className="text-gray-100 font-bold text-3xl py-3 ">YT⬇ | Bem vindo!</h2>
         </header>
 
         <section className="flex justify-center my-5 ">
-          <div className="w-full mx-2 md:w-9/12 lg:w-6/12 xl:w-5/12">
-            <SearchBar setSearchResult={setSearchResult} />
+          <div className="w-full md:w-9/12 lg:w-6/12 xl:w-5/12">
+            <SearchBar setSearchResult={setSearchResult}/>
           </div>
-
         </section>
 
-        <section className="mt-5 mx-2 rounded-lg shadow-md md:w-8/12 md:mx-auto">
+        {!searchResults && [true].map((val, key)=>{
+          return (<div key={key} className="bg-red absolute inset-1/4">
+
+            
+            <div className="text-white text-xl text-center border-4 border-gray-700 border-dashed">
+              <div className="p-6">
+                <p className="text-yellow-500 font-black text-2xl text-center">AVISO:</p>  
+                <p> os arquivos de <span className="animate-pulse text-pink-300">audio</span> fornecidos pelo ytDown estão no formato <code className="font-black text-red-400">.webm</code></p>
+                <p>Estamos trabalhando em um conversor rapido e pratico para o formato<code className="font-black text-green-400">.mp3</code></p>
+              </div>
+            </div>
+          
+          </div>)
+        })}
+
+
+        <section className="mt-5 mx-1 rounded-lg shadow-md">
           {searchResults && searchResults.map((val, key)=>{
             if (val.type !== 'video'|| val.duration == null) return;
+
             return <VideoSelector
             title={val.title} url={val.url} key={key}
-            author={val.author} duration={val.duration} description={val.description} thumbnail={val.bestThumbnail.url}/>
+            author={val.author} duration={val.duration} thumbnail={val.bestThumbnail.url}/>
           })}
         </section>
-      </main>
 
+        <footer className="flex items-end rounded-t-lg h-screen -mt-36">
+          <div className="bg-gray-800 py-7 -mt-16 w-full text-center text-gray-100 text-xl font-bold">
+            <p className="animate-bounce">João Vitor Cruz</p>
+
+          <div className="flex justify-center mt-2">
+            <a href="https://www.github.com/JoaoVitorCruz">
+              <GitHubIcon className="bg-gray-900 cursor-pointer" />
+            </a>
+            {'   '}
+            <a href="https://wa.link/84h3sj">
+              <WhatsappIcon className=" bg-gray-100 text-green-600 mx-3 cursor-pointer"/>
+            </a>
+            {'   '}
+            <a href="https://www.instagram.com/proxy_167/">
+              <InstagramIcon className="bg-gradient-to-r from-yellow-400 via-red-500 cursor-pointer to-pink-500"/>
+            </a>
+          </div>
+            
+          </div>
+        </footer>
       
     </div>
   )
